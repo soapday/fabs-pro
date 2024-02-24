98271a583b58c47e65a834f77a84031ae5b12b96
Sabda <sabdajauharahibrizi@gmail.com>
2024-02-24 09:59:23 +0700

    lasted update sabda sebelum pulang

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,8 @@
+{{-- include ini biar nyambung sama title / head dari web  --}}
 @include('assets.base')
+{{-- extend ini buat nyambungin sama navbar --}}
 @extends('assets.navbar')
+
 @section('content')
-    
+    {{-- Nanti apip Mulai ngoding bikin halaman webnya disini --}}
 @endsection
