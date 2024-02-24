171624c6db0f509f41f2445f2767a01cf719a89a
Apip <afifkawakib8@gmail.com>
2024-02-24 09:18:41 +0700

    commit dari apip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
     <title>Fabulous</title>
 </head>
 <body>
     <li>Home</li>
     <li>About us</li>
     <li>Customer Servis</li>
+
 </body>
 </html>
