18218b7c050f3f3f12f517722a3bbe5ce1ff4b20
Sabda <sabdajauharahibrizi@gmail.com>
2024-02-24 07:23:57 +0700

    sabda day 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Halo</title>
-</head>
-<body>
-    <h1>HALO</h1>
-    <h2>kenapa </h2>
-    <p>gapapa</p>
-    <nav class="navbar-brand">
-        
-    </nav>
-</body>
-</html>
+@extends('assets.base')
+
+@include('assets.navbar')
+@content
