19ce4472ac9986e91f3780d7f9ca3d93deb49c4d
apip <afifkawakib8@gmail.com>
2024-02-28 21:31:41 +0700

    perubahan pada welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,18 @@
 
 =======
 {{-- include ini biar nyambung sama title / head dari web  --}}
+<title>Fabulous</title>
 @include('assets.base')
 {{-- extend ini buat nyambungin sama navbar --}}
+<lu>
+    <li>Home</li>
+    <li></li>
+    <li></li>
+    <li></li>
+</lu>
 @extends('assets.navbar')
 >>>>>>> 98271a583b58c47e65a834f77a84031ae5b12b96
 
 @section('content')
     {{-- Nanti apip Mulai ngoding bikin halaman webnya disini --}}
-    <div></div>
 @endsection
