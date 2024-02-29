dc2627b996a5b5a622be2ed0de96475bb75ebb14
Apip <afifkawakib8@gmail.com>
2024-02-29 08:42:23 +0700

    perubahan pada public adanya aset css dan navbar saya hapus dan saya buat di base blase

--- resources/views/assets/base.blade.php
+++ resources/views/assets/base.blade.php
@@ -4,9 +4,49 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
-  <body>
+    <link rel="stylesheet" href="style.css">
+    </head>
+  <body class="body">
+    <nav class="nav">
+         <ul class="navul">
+            <li class="navli">
+                <a class="navhome" href=#>Home</a>
+            </li>
+            <li class="navli">
+                <a class="navpro" href=#>Product</a>
+            </li>
+            <li class="navli">
+                <a class="navabout" href=#>About us</a>
+            </li>
+        </ul>
+    </nav>
+    {{-- Style Navbar --}}
+    <style>
+        body {
+            background-color: hsl(218, 44%, 59%);
+            margin: 0;
+            padding: 0;
+            font-family: Arial, sans-serif;
+
+        }
+        ul{
+
+        }
+
+        li {
+            background-color: #ffffff;
+            list-style: none;
+            display: flex;
+            position: end;
+        }
+        a {
+            text-align: center;
+            padding: 50px 60px;
+            margin: 50px 60px;
+        }
+
+
+    </style>
     @yield('content')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,23 +1,14 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 <!DOCTYPE html>
-{{-- dgdxcg --}}
+{{-- Title WEB --}}
 
-=======
-{{-- include ini biar nyambung sama title / head dari web  --}}
-<title>Fabulous</title>
-=======
->>>>>>> 79e1e802de86ea7e719ba9adeeea80e1c51e1602
+{{-- NAVBAR --}}
+
+
 @include('assets.base')
 {{-- extend ini buat nyambungin sama navbar --}}
-<lu>
-    <li>Home</li>
-    <li></li>
-    <li></li>
-    <li></li>
-</lu>
 @extends('assets.navbar')
 
 @section('content')
+
     {{-- Nanti apip Mulai ngoding bikin halaman webnya disini --}}
 @endsection
